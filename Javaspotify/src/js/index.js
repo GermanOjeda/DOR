0c048950a03bc5d16cb85f98e91d9c157abbea94
GermanOjeda <germanadolfoop@gmail.com>
2024-09-26 15:32:10 +0100

    index.js started

--- Javaspotify/src/js/index.js
+++ Javaspotify/src/js/index.js
@@ -1,1 +1,3 @@
-const audio1 = new Audio("../assets/songs/Baka_Mitai.mp3")
+const audio1 = new Audio("../assets/songs/Baka_Mitai.mp3");
+const audio2 = new Audio("../assets/songs/In_Full_Blast.mp3");
+const audio3 = new Audio("../assets/songs/Yi_jian_mei.mp3");
